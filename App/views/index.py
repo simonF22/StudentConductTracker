07f464ad451d12720989fd219dd817cd5a02af61
simonF22 <marc.ramatali@outlook.com>
2023-12-02 05:53:48 +0000

    fixed /init route

--- App/views/index.py
+++ App/views/index.py
@@ -16,17 +16,21 @@ def generate_random_contact_number():
     return f"0000-{random.randint(100, 999)}-{random.randint(1000, 9999)}"
 
 
-@index_views.route('/init', methods=['POST'])
+@index_views.route('/init', methods=['GET'])
 def init():
-    db.drop_all()
-    db.create_all()
-    admin= create_admin('bob', 'boblast' , 'bobpass')
-      
-    staff = create_staff(admin, '0012', 'John', 'Mann,' 'johnpass', 'johnmann@schooling.com')
-    staff = create_staff(admin, '0013', 'Jane', 'Anne,' 'janepass', 'janeanne@schooling.com')
 
-    student = create_student(admin, '0021', 'Nick', 'Dell', generate_random_contact_number(), random.choice(['Full-Time','Part-Time', 'Evening']), str(random.randint(1, 8)))
-    student = create_student(admin, '0022', 'John', 'Biz', generate_random_contact_number(), random.choice(['Full-Time','Part-Time', 'Evening']), str(random.randint(1, 8)))
-    
+    try:
+        db.drop_all()
+        db.create_all()
+        admin= create_admin('bob', 'boblast' , 'bobpass')
+            
+        staff = create_staff(admin, '0012', 'John', 'Mann,' 'johnpass', 'johnmann@schooling.com')
+        staff = create_staff(admin, '0013', 'Jane', 'Anne,' 'janepass', 'janeanne@schooling.com')
 
-    return jsonify({'message': 'Database initialized'}),201
+        student = create_student(admin, '0021', 'Nick', 'Dell', generate_random_contact_number(), random.choice(['Full-Time','Part-Time', 'Evening']), str(random.randint(1, 8)))
+        student = create_student(admin, '0022', 'John', 'Biz', generate_random_contact_number(), random.choice(['Full-Time','Part-Time', 'Evening']), str(random.randint(1, 8)))
+        
+        return jsonify(message='db initialized!')
+
+    except Exception as e:
+        return jsonify({'success': False, 'message': f'Error initializing application: {str(e)}'})
